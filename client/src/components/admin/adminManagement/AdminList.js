10756ab52eed9eee56a72cd3f460285366d12042
MaxLacasseG <max.lacasse.g@gmail.com>
2019-01-16 16:44:35 -0500

    Update admin

--- client/src/components/admin/adminManagement/AdminList.js
+++ client/src/components/admin/adminManagement/AdminList.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import AdminListItem from "./AdminListItem";
+import UpdateAdminModal from "./UpdateAdminModal";
 import { GetAllAdmins, DeleteAdmin } from "../../../store/actions/adminActions";
 import { ClearResponse } from "../../../store/actions/responseActions";
 
 import PropTypes from "prop-types";
 
 class AdminList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedAdmin: {},
+            modal: ""
+        };
+        this.initialState = this.state;
+    }
+
     componentDidMount = () => {
         this.props.GetAllAdmins();
     };
@@ -18,16 +28,31 @@ class AdminList extends Component {
         this.props.DeleteAdmin(admin._id);
     };
 
-    UpdateAdmin = admin => {
-        //Call update action
-        console.log("Update", admin);
+    ClearModal = () => {
+        this.setState({ modal: "" });
+    };
+
+    ShowUpdateModal = admin => {
+        this.setState({ selectedAdmin: admin }, () => {
+            const modal = <UpdateAdminModal admin={this.state.selectedAdmin} UpdateAdmin={this.UpdateAdmin} ClearModal={this.ClearModal} />;
+            this.setState({ modal }, () => {
+                //Show modal box
+                document.getElementById("updateModalBtn").click();
+            });
+        });
+    };
+
+    UpdateAdmin = newAdminInfos => {
+        console.log("Update", newAdminInfos);
     };
     render() {
         const action = this.props.action;
         const adminList = this.props.admin.adminList;
+
         const adminListItems = adminList.map(admin => {
-            return <AdminListItem admin={admin} UpdateAdmin={this.UpdateAdmin} DeleteAdmin={this.DeleteAdmin} key={admin._id} />;
+            return <AdminListItem admin={admin} UpdateAdmin={this.ShowUpdateModal} DeleteAdmin={this.DeleteAdmin} key={admin._id} />;
         });
+
         const successMessage = (
             <div className="alert alert-success alert-dismissible fade show mt-3" role="alert">
                 Administrateur supprimé
@@ -38,6 +63,15 @@ class AdminList extends Component {
         );
         return (
             <div className="p-5">
+                <button
+                    id="updateModalBtn"
+                    className="icon-button"
+                    data-target="#updateAdminModal"
+                    data-toggle="modal"
+                    style={{ display: "hidden" }}
+                />
+                {this.state.modal}
+
                 <h4 className="section-header">
                     <i className="fas fa-list-ul" />
                     {"  "}
---
+++ client/src/components/admin/adminManagement/UpdateAdminModal.js
@@ -0,0 +1,223 @@
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+
+/**
+ * @props UpdateAdmin(newAdminInfos)    function    Updates the admin profile
+ * @props ClearModal                    function    Reset modal state in parent
+ * @props admin                         object      the admin
+ */
+class UpdateAdminModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            pwd: "",
+            pwd2: "",
+            firstName: "",
+            lastName: "",
+            phone: "",
+            organization: "",
+            isAdmin: false
+        };
+        this.initialState = this.state;
+    }
+
+    componentDidMount = () => {
+        console.log("Modal mount");
+        this.SetAdminInfo(this.props.admin);
+    };
+    componentWillUnmount = () => {
+        console.log("Modal unmount");
+    };
+
+    SetAdminInfo = admin => {
+        this.setState(admin);
+    };
+
+    UpdateAdmin = e => {
+        document.getElementById("closeModalBtn").click();
+        this.props.UpdateAdmin(this.state);
+    };
+
+    ClearForm = () => {
+        this.setState(this.initialState);
+        this.props.ClearModal();
+    };
+
+    OnSubmit = e => {
+        e.preventDefault();
+    };
+    OnChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    OnCheck = e => {
+        this.setState({ [e.target.name]: e.target.checked });
+    };
+
+    OnSelect = e => {
+        this.setState({ [e.target.name]: e.target.selectedOptions[0].value });
+    };
+    render() {
+        const errors = this.props.errors;
+        const organizationList = [
+            "Réseau Technoscience",
+            "Technoscience Abitibi-Témiscamingue",
+            "Technoscience Est-du-Québec",
+            "Technoscience Saguenay–Lac-Saint-Jean",
+            "Technoscience Mauricie, Centre-du-Québec",
+            "Technoscience Estrie",
+            "Technoscience Région métropolitaine",
+            "Technoscience Côte-Nord",
+            "Technoscience Outaouais",
+            "Boite à science",
+            "AEST"
+        ];
+        const organizationOptions = organizationList.map(org => {
+            return (
+                <option value={org} key={org}>
+                    {org}
+                </option>
+            );
+        });
+        return (
+            <Fragment>
+                <div className="container">
+                    <div className="modal fade" id="updateAdminModal" role="dialog" data-backdrop="static" data-keyboard="false">
+                        <div className="modal-dialog modal-lg">
+                            <div className="modal-content">
+                                <div className="modal-body">
+                                    {/* ======== */}
+                                    {/* FORM BEGINNING */}
+                                    {/* ======== */}
+                                    <form onSubmit={this.OnSubmit}>
+                                        <div className="row">
+                                            <div className="col-4">
+                                                <h5>Informations</h5>
+                                            </div>
+                                            <div className="col-8">
+                                                <div className="form-check form-check-inline">
+                                                    <input
+                                                        className="form-check-input"
+                                                        type="checkbox"
+                                                        id="isAdmin"
+                                                        name="isAdmin"
+                                                        onChange={this.OnCheck}
+                                                        checked={this.state.isAdmin}
+                                                    />
+                                                    <label className="form-check-label" htmlFor="isAdmin">
+                                                        Est super administrateur?
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="email">Nom d'utilisateur</label>
+                                            <input
+                                                type="text"
+                                                className={classnames("form-control", {
+                                                    "is-invalid": errors.email
+                                                })}
+                                                name="email"
+                                                id="email"
+                                                placeholder="Courriel"
+                                                onChange={this.OnChange}
+                                                value={this.state.email}
+                                            />
+                                            {errors.email && <div className="invalid-feedback">{errors.email}</div>}
+                                        </div>
+                                        <hr />
+                                        <div className="form-group">
+                                            <label htmlFor="firstName">Prénom</label>
+                                            <input
+                                                type="text"
+                                                className={classnames("form-control", {
+                                                    "is-invalid": errors.firstName
+                                                })}
+                                                name="firstName"
+                                                id="firstName"
+                                                placeholder="Prénom"
+                                                onChange={this.OnChange}
+                                                value={this.state.firstName}
+                                            />
+                                            {errors.firstName && <div className="invalid-feedback">{errors.firstName}</div>}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="lastName">Nom de famille</label>
+                                            <input
+                                                type="text"
+                                                className={classnames("form-control", {
+                                                    "is-invalid": errors.lastName
+                                                })}
+                                                name="lastName"
+                                                id="lastName"
+                                                placeholder="Nom de famille"
+                                                onChange={this.OnChange}
+                                                value={this.state.lastName}
+                                            />
+                                            {errors.lastName && <div className="invalid-feedback">{errors.lastName}</div>}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="phone">Numéro de téléphone à contacter durant la finale</label>
+                                            <input
+                                                type="tel"
+                                                className={classnames("form-control", {
+                                                    "is-invalid": errors.phone
+                                                })}
+                                                id="phone"
+                                                name="phone"
+                                                pattern="[0-9]{3} [0-9]{3}-[0-9]{4}"
+                                                placeholder="222 222-2222"
+                                                onChange={this.OnChange}
+                                                value={this.state.phone}
+                                            />
+                                            {errors.phone && <div className="invalid-feedback">{errors.phone}</div>}
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="organization">Organisme régional</label>
+                                            <select
+                                                className={classnames("form-control custom-select", {
+                                                    "is-invalid": errors.organization
+                                                })}
+                                                name="organization"
+                                                id="organization"
+                                                value={this.state.organization}
+                                                onChange={this.OnSelect}
+                                            >
+                                                <option value="">Choisir un organisme</option>
+                                                {organizationOptions}
+                                            </select>
+                                            {errors.organization && <div className="invalid-feedback">{errors.organization}</div>}
+                                        </div>
+                                        <hr />
+                                        <button className="btn mr-3" onClick={this.UpdateAdmin}>
+                                            <i className="fas fa-save" /> Modifier l'utilisateur
+                                        </button>
+                                        <button id="closeModalBtn" className="btn" data-dismiss="modal" onClick={this.ClearForm}>
+                                            <i className="fas fa-times" /> Annuler
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    action: state.action,
+    errors: state.errors
+});
+
+UpdateAdminModal.propTypes = {
+    admin: PropTypes.object.isRequired,
+    UpdateAdmin: PropTypes.func.isRequired,
+    ClearModal: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps)(UpdateAdminModal);
