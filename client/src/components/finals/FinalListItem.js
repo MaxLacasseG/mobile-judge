e79b5921f21eff22d7af9b28ef4ebab6d2199e47
Maxime Lacasse-Germain <mlacassegermain@MacBook-Pro-de-Reseau.local>
2019-01-24 17:06:55 -0500

    Corr

--- client/src/components/finals/FinalListItem.js
+++ client/src/components/finals/FinalListItem.js
@@ -8,49 +8,66 @@ import PropTypes from "prop-types";
  * @props object    final               the final infos
  */
 class FinalListItem extends Component {
-    ToggleActivationFinal = e => {
-        if (this.props.final !== null && this.props.final !== undefined) {
-            this.props.ToggleActivationFinal(this.props.final._id);
-        } else {
-            console.log("Err ToggleActivationFinal():No final found");
-        }
-    };
-    GoTo = address => {
-        this.props.history.push(address);
-    };
-    RenderDate = date => {
-        const renderedDate = new Date(Date.parse(date)).toLocaleDateString("fr-CA", { timeZone: "UTC" });
-        return renderedDate;
-    };
-    render() {
-        const { _id, longName, isActive, level, eventDate } = this.props.final;
-        return (
-            <div className="row border-bottom">
-                <div className="col-9">
-                    <p className="p-0 m-0">
-                        {longName}- {isActive ? <span className="text-info">Actif</span> : <span className="text-muted">Inactif</span>}
-                    </p>
-                    <small>
-                        Volet {level === "elementary" ? "primaire" : level === "highschool" ? "secondaire" : null} - {this.RenderDate(eventDate)}
-                    </small>
-                </div>
-                <div className="col d-flex flew-row justify-content-around align-items-start">
-                    <button className="icon-button" onClick={this.ToggleActivationFinal}>
-                        {isActive ? <i className="fas fa-2x fa-toggle-on text-info" /> : <i className="fas fa-2x fa-toggle-off text-muted" />}
-                    </button>
+	ToggleActivationFinal = e => {
+		if (this.props.final !== null && this.props.final !== undefined) {
+			this.props.ToggleActivationFinal(this.props.final._id);
+		} else {
+			console.log("Err ToggleActivationFinal():No final found");
+		}
+	};
+	GoTo = address => {
+		this.props.history.push(address);
+	};
+	RenderDate = date => {
+		const renderedDate = new Date(Date.parse(date)).toLocaleDateString("fr-CA", {
+			timeZone: "UTC"
+		});
+		return renderedDate;
+	};
+	render() {
+		const { _id, longName, isActive, level, eventDate } = this.props.final;
+		return (
+			<div className="row border-bottom">
+				<div className="col-9">
+					<p className="p-0 m-0">
+						{longName}-{" "}
+						{isActive ? (
+							<span className="text-info">Actif</span>
+						) : (
+							<span className="text-muted">Inactif</span>
+						)}
+					</p>
+					<small>
+						Volet{" "}
+						{level === "elementary"
+							? "primaire"
+							: level === "highschool"
+							? "secondaire"
+							: null}{" "}
+						- {this.RenderDate(eventDate)}
+					</small>
+				</div>
+				<div className="col d-flex flew-row justify-content-around align-items-start">
+					<button className="icon-button" onClick={this.ToggleActivationFinal}>
+						{isActive ? (
+							<i className="fas fa-2x fa-toggle-on text-info" />
+						) : (
+							<i className="fas fa-2x fa-toggle-off text-muted" />
+						)}
+					</button>
 
-                    <Link to={`/admin/finale/${_id}`} className="icon-button">
-                        <i className="fas fa-2x fa-sign-out-alt" />
-                    </Link>
-                </div>
-            </div>
-        );
-    }
+					<Link to={`/admin/finale/${_id}`} className="icon-button">
+						<i className="fas fa-2x fa-sign-out-alt" />
+					</Link>
+				</div>
+			</div>
+		);
+	}
 }
 
 FinalListItem.propTypes = {
-    final: PropTypes.object.isRequired,
-    ToggleActivationFinal: PropTypes.func.isRequired
+	final: PropTypes.object.isRequired,
+	ToggleActivationFinal: PropTypes.func.isRequired
 };
 
 export default withRouter(FinalListItem);
