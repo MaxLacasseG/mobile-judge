a91b66f1feacf8ae63d1794ac3774097a16accef
Maxime Lacasse-Germain <mlacassegermain@MacBook-Pro-de-Reseau.local>
2019-02-21 16:55:10 -0500

    style btn ranking

--- client/src/components/finals/finalPanels/FinalViewExportation.js
+++ client/src/components/finals/finalPanels/FinalViewExportation.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import FinalNav from "../../pages/partials/FinalNav";
 
 import { SelectFinalById } from "../../../store/actions/finalActions";
+import { SelectProjectsByFinalId } from "../../../store/actions/projectActions";
 import isEmpty from "../../../validation/isEmpty";
 
 class FinalViewExportation extends Component {
@@ -14,30 +15,38 @@ class FinalViewExportation extends Component {
 		};
 	}
 
+	componentDidMount = () => {
+		this.props.SelectFinalById(this.props.match.params[0]);
+		this.props.SelectProjectsByFinalId(this.props.match.params[0]);
+	};
+
 	CalculateResults = results => {
 		for (let project in results) {
-			//this.CalculateProjectResults(project, results[project][judge]);
-			console.log(project, results[project]);
+			this.CalculateProjectResults(project, results[project]);
 		}
 	};
 
 	CalculateProjectResults = (projectNumber, projectResults) => {
-		//console.log("final results", projectNumber, projectResults);
+		console.log("final results", projectNumber, projectResults);
 		const resultArray = Object.keys(projectResults).map(key => {
 			return projectResults[key].total;
 		});
 
-		//Calc average
 		const results = this.state.results;
-
 		if (isEmpty(results[projectNumber])) {
 			results[projectNumber] = {};
 		}
+		results[projectNumber].individualResults = projectResults;
+
+		//Calc average
 		results[projectNumber].finalAvgResults = this.CalcAvg(resultArray);
 
 		//Calc trimmed average
 		results[projectNumber].finalTrimmedAvgResults = this.CalcTrimmedAvg(resultArray);
-		this.setState({ results });
+
+		this.setState({ results }, () => {
+			this.SortProjectByDefault();
+		});
 	};
 
 	/**
@@ -101,6 +110,24 @@ class FinalViewExportation extends Component {
 		this.setState({ sortedResults });
 	};
 
+	SortProjectByDefault = () => {
+		let results = this.state.results;
+		if (isEmpty(results)) return;
+
+		let ranking = [];
+		ranking = Object.keys(results).sort((a, b) => {
+			return results[b].finalAvgResults - results[a].finalAvgResults;
+		});
+
+		this.ResetDirection();
+
+		const defaultBtn = document.querySelector(".sort-default");
+		defaultBtn.classList.add("sort-active");
+		this.FlipIconDirection(defaultBtn);
+
+		this.SortByRanking(results, ranking);
+	};
+
 	SortProjectByProjectNumber = e => {
 		let results = this.state.results;
 		if (isEmpty(results)) return;
@@ -118,7 +145,10 @@ class FinalViewExportation extends Component {
 			});
 		}
 
+		this.ResetDirection();
 		e.target.dataset.direction = direction * -1;
+		e.target.classList.add("sort-active");
+		this.FlipIconDirection(e.target);
 		this.SortByRanking(results, ranking);
 	};
 
@@ -138,7 +168,10 @@ class FinalViewExportation extends Component {
 				return results[b].finalAvgResults - results[a].finalAvgResults;
 			});
 		}
+		this.ResetDirection();
 		e.target.dataset.direction = direction * -1;
+		e.target.classList.add("sort-active");
+		this.FlipIconDirection(e.target);
 		this.SortByRanking(results, ranking);
 	};
 
@@ -158,9 +191,26 @@ class FinalViewExportation extends Component {
 				return results[b].finalTrimmedAvgResults - results[a].finalTrimmedAvgResults;
 			});
 		}
-
+		this.ResetDirection();
 		e.target.dataset.direction = direction * -1;
+		e.target.classList.add("sort-active");
+		this.FlipIconDirection(e.target);
 		this.SortByRanking(results, ranking);
+	};
+
+	FlipIconDirection = elem => {
+		elem.classList.remove("sort-up", "sort-down");
+		elem.dataset.direction == 1
+			? elem.classList.add("sort-up")
+			: elem.classList.add("sort-down");
+	};
+
+	ResetDirection = () => {
+		document.querySelectorAll(".btn-sort").forEach(elem => {
+			elem.classList.remove("sort-active");
+			elem.dataset.direction = 1;
+			this.FlipIconDirection(elem);
+		});
 	};
 
 	render() {
@@ -171,10 +221,21 @@ class FinalViewExportation extends Component {
 			? []
 			: this.props.final.selectedFinal.results;
 
-		const resultList = this.state.sortedResults.map(result => {
+		const list = this.state.sortedResults.map((result, index) => {
+			let number, avg, trimmedavg;
+
+			Object.keys(result).map(key => {
+				number = key;
+				avg = result[key].finalAvgResults;
+				trimmedavg = result[key].finalTrimmedAvgResults;
+				return true;
+			});
+
 			return (
-				<div>
-					<div>1</div>
+				<div className="col-12 row ranking-row my-2 py-3" key={index}>
+					<div className="col-1 text-center">{index + 1}</div>
+					<div className="col-3 text-center">{number}</div>
+					<div className="col-6 text-center">{trimmedavg.toFixed(6)}</div>
 				</div>
 			);
 		});
@@ -182,51 +243,56 @@ class FinalViewExportation extends Component {
 		return (
 			<Fragment>
 				<FinalNav pageTitle="Finale - Exportation" id={id} finalName={final.longName} />
-				<div className="container ">
+				<div className="container">
 					<div className="row">
-						<div className="mx-auto">
+						<div className="mx-auto text-center">
 							<h1>Exporter vos données</h1>
-							<button
-								type="button"
-								className="btn btn-reseau"
-								onClick={() => {
-									this.CalculateResults(results);
-								}}
-							>
-								Calculer note finale
-							</button>
+						</div>
+						<div className="row my-3 col-12">
+							<div className="col-6 mx-auto text-center">
+								<button
+									type="button"
+									className="btn btn-reseau btn-lg btn-block p-3"
+									onClick={() => {
+										this.CalculateResults(results);
+									}}
+								>
+									<span className="text-uppercase font-weight-bold">
+										Calculer le classement
+									</span>
+								</button>
+							</div>
+						</div>
+						<div className="d-flex justify-content-around align-items-center col-12 my-3">
 							{/* SORTING BTNS */}
-							<button
-								type="button"
-								className="btn btn-reseau"
-								onClick={this.SortProjectByProjectNumber}
-								data-direction="1"
-							>
-								Trier par projet
-							</button>
-							<button
-								type="button"
-								className="btn btn-reseau"
-								onClick={this.SortProjectByAvg}
-								data-direction="-1"
-							>
-								Trier par moyenne
-							</button>
-							<button
-								type="button"
-								className="btn btn-reseau"
-								onClick={this.SortProjectByTrimmedAvg}
-								data-direction="-1"
-							>
-								Trier par moyenne intermédiaire
-							</button>
+							<div>
+								<button
+									type="button"
+									className="btn btn-reseau btn-sort sort-down"
+									onClick={this.SortProjectByProjectNumber}
+									data-direction="1"
+								>
+									Trier par numéro de projet
+								</button>
+							</div>
+							<div>
+								<button
+									type="button"
+									className="btn btn-reseau btn-sort sort-up sort-active sort-default"
+									onClick={this.SortProjectByTrimmedAvg}
+									data-direction="-1"
+								>
+									Trier par résultat
+								</button>
+							</div>
 						</div>
 					</div>
 					<div className="row">
-						<div className="col">Projet</div>
-						<div className="col">Moyenne</div>
-						<div className="col">Moyenne intermédiaire</div>
+						<div className="col-1 text-center">Ordre</div>
+						<div className="col-3 text-center">Numéro de projet</div>
+						<div className="col-6 text-center">Résultat</div>
 					</div>
+					<div className="row mb-5">{list}</div>
 				</div>
 			</Fragment>
 		);
@@ -240,5 +306,5 @@ const mapStateToProps = state => ({
 
 export default connect(
 	mapStateToProps,
-	{ SelectFinalById }
+	{ SelectFinalById, SelectProjectsByFinalId }
 )(FinalViewExportation);
