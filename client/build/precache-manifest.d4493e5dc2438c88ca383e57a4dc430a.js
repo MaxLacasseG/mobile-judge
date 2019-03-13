f6f1d68f5844e73965df78029173b2d2a50ec020
Maxime Lacasse-Germain <mlacassegermain@MacBook-Pro-de-Reseau.local>
2019-03-13 13:25:25 -0400

    New build

--- client/build/precache-manifest.d4493e5dc2438c88ca383e57a4dc430a.js
+++ client/build/precache-manifest.d4493e5dc2438c88ca383e57a4dc430a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "570de2d52f1a4e833baa",
+    "revision": "7de539b5eea96a7515b6",
     "url": "/static/css/main.e5f44287.chunk.css"
   },
   {
-    "revision": "570de2d52f1a4e833baa",
-    "url": "/static/js/main.570de2d5.chunk.js"
+    "revision": "7de539b5eea96a7515b6",
+    "url": "/static/js/main.7de539b5.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.4e819ed2.chunk.js"
   },
   {
-    "revision": "c755832247dcc3ddd81bd579d1678734",
+    "revision": "ffefbdec14b4391a815b152b5d3699ef",
     "url": "/index.html"
   }
 ];
